4fd63a446d95b641161df2776b9ff9e545173f48
siam-26 <siamshahriar390@gmail.com>
2022-12-11 09:51:35 +0600

    navbar designed

---
+++ src/Pages/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <div className="navbar bg-secondary">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                        </label>
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                            <li><a>Home</a></li>
+                            <li><a>About</a></li>
+                            <li><a>Skills</a></li>
+                            <li><a>Projects</a></li>
+                            <li><a>Contact</a></li>
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost normal-case text-xl font-semibold text-white pl-0 md:pl-48 lg:pl-16">Sayed Abu Ahmad</a>
+                </div>
+                <div className="navbar-end hidden lg:flex pr-16">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><a className='text-white'>Home</a></li>
+                        <li><a className='text-white'>About</a></li>
+                        <li><a className='text-white'>Skills</a></li>
+                        <li><a className='text-white'>Projects</a></li>
+                        <li><a className='text-white'>Contact</a></li>
+                    </ul>
+                </div>
+                <div className='pl-16 md:pl-48 lg:pr-16'>
+                    <a className="btn">My Resume</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          primary: "#cd4b2e",
+
+          secondary: "#cd4b2ebf",
+
+          accent: "#d6d6d6",
+
+          neutral: "#676767",
+
+          "base-100": "#FFFFFF",
+
+          "info": "#3ABFF8",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
